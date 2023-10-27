9981841058bc5ad4463c35f6270f49dc0534e927
SWZ <contact@swz.works>
2023-10-27 19:22:58 +0200

    fix 'VCRUNTIME140.dll missing'

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 extern crate winres;
 
 fn main() {
+    static_vcruntime::metabuild();
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/logo.ico");
